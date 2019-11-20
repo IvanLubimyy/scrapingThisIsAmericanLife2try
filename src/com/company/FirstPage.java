5cab26394c77e636e335df9c88fcda4982c9797d
IvanLubimyy <shiva01@yandex.ru>
2019-11-20 22:49:40 +0400

    do some refactoring, need do it more jet

--- src/com/company/FirstPage.java
+++ src/com/company/FirstPage.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+/*
+Class which can wait fool download the FirstPage
+ */
 public class FirstPage {
 
     private WebDriver driver;
@@ -15,7 +17,9 @@ public class FirstPage {
         this.driver = driver;
     }
 
-
+/*
+method can wait SomeLocatorByXpath on the page
+ */
     public void waiter(String SomeLocatorByXpath){
         WebDriverWait waitForOne = new WebDriverWait(driver, 10);
         waitForOne.until(ExpectedConditions.presenceOfElementLocated(By.xpath(SomeLocatorByXpath)));
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,36 @@ import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
-import java.net.URL;
-import java.util.ArrayList;
 
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 //import org.junit.*;
 
-
+/*
+Main class where we can
+1) go to targetPage
+2) find, wait and collect linksOfEpisodes
+3) download with workingStreams number of treads , wait and checkExisting mp3 files
+ */
 public class Main {
+        public List<String> linksOfEpisodes= new ArrayList<String>(); //list of episodes we will download
         public WebDriver driver = new FirefoxDriver();
+        public String targetPage = "https://www.thisamericanlife.org/archive?year=2018";
+        public int workingStreams = 10;
+        public String localPlace = "c:\\work\\1";
 
         private final Wait<WebDriver> wait = new WebDriverWait(driver, 20, 1000);
     /**
          * Open the test website.
          */
         public void openTestSite() { //view-source:
-            driver.navigate().to("https://www.thisamericanlife.org/archive?year=2018");
+            driver.navigate().to(targetPage);
         }
 
         /*
@@ -32,15 +42,12 @@ public class Main {
          *            password
          */
         public void login(String username, String Password) {
-
             WebElement userName_editbox = driver.findElement(By.id("usr"));
             WebElement password_editbox = driver.findElement(By.id("pwd"));
             WebElement submit_button = driver.findElement(By.xpath("//input[@value='Login']"));
-
             userName_editbox.sendKeys(username);
             password_editbox.sendKeys(Password);
             submit_button.click();
-
         }
 
         public void closeCookieWindow() throws IOException {
@@ -69,58 +76,76 @@ public class Main {
     }
     /*
      * find all "thumbnail goto goto-episode" classes on the page
+     * return true if this list not null
      */
-    public void findAllThumbnails() throws IOException, InterruptedException {
-        List<String> linksOfEpisodes= new ArrayList<String>();
-        FirstPage episodePage = new FirstPage(driver);
-        episodePage.waiter("//*[@class='thumbnail goto goto-episode']");
+    public boolean findAllThumbnails() throws IOException, InterruptedException {
+        //FirstPage episodePage = new FirstPage(driver);
+        waiter("//*[@class='thumbnail goto goto-episode']");
 
         List<WebElement> links = driver.findElements(By.cssSelector("a.thumbnail.goto.goto-episode"));
         // remember all links before we will update page
-        for (WebElement link : links) {
-            if (link==null) { System.out.println("Empty list"); return; }
-            else {linksOfEpisodes.add(link.getAttribute("href").toString());}
-        }
-        // download audio from all pages
-        for (String Episode :linksOfEpisodes ) {
-            driver.navigate().to(new URL(Episode));
-            playEpisode();
+        if (links.isEmpty()){
+            System.out.println("Empty list");
+            return false;
         }
 
+        for (WebElement link : links) {
+           linksOfEpisodes.add(link.getAttribute("href").toString());
+        }
+        return true;
+    }
+
+    // download audio from all pages
+    public void downloadList() throws IOException, InterruptedException {
+        for (String Episode :linksOfEpisodes ) {
+            driver.navigate().to(new URL(Episode));
+            Thread taskToDownload = playEpisode();
+            if(taskToDownload ==null){
+                System.out.println("Episode "+Episode+" does not have audio content");
+            }else{
+//todo add counter of treads
+                System.out.println("Download " +taskToDownload.getName()+ " ends. Congrats!");
+            }
+        }
     }
 
         /*
         Find and play episode
         */
-         public void playEpisode() throws IOException, InterruptedException {
-            FirstPage episodePage = new FirstPage(driver);
-            episodePage.waiter("//*[@id='playlist-data']");
+         public Thread playEpisode() throws IOException, InterruptedException {
+            //Page episodePage = new FirstPage(driver);
+             waiter("//*[@id='playlist-data']");
              //List<WebElement> playEpisodes = driver.findElements(By.xpath("//script"));
              WebElement playEpisode = driver.findElement(By.xpath("//script[contains(@id,'playlist-data')]"));
+             if (playEpisode==null){
+                 return null;
+             }
              System.out.println("Нажал: tag:" + playEpisode.getTagName()+" id:"+playEpisode.getAttribute("id")+" text:"+playEpisode.getAttribute("text")+" title:"+playEpisode.getAttribute("title"));
-//Json work
+             //Json work
              String str = playEpisode.getAttribute("text");
              JSONObject obj = new JSONObject(str);
              String mp3File = obj.getString("audio");
              System.out.println("Start downloading:"+mp3File);
              //download mp3
-//todo add limit of the number of sessions
-//todo remove additional waiting
-//todo refactoring (think about architecture and UI)
-//todo add Singleton for logging
-             Thread audioFile = new Thread(new DownLoadURL(mp3File, "c:\\work\\1"));
+
+             Thread audioFile = new Thread(new DownLoadURL(mp3File, localPlace));
              try {
                  audioFile.sleep(3000);
                  audioFile.start();
-                 System.out.println("Download " + mp3File + " ends. Congrats!");
-                 audioFile.sleep(180000); // wait 3 minutes to start new download tread
+                 audioFile.setName(mp3File);
+                 System.out.println("Download " + mp3File + " starts");
+
+                 //audioFile.sleep(180000); // wait 3 minutes to start new download tread
              } catch(InterruptedException e) {
                  e.printStackTrace();
+                 System.out.println("We have a problem with start downloading the file "+ mp3File);
+                 return null;
              }
-                 //}
-             //}
-            //playEpisod.click();
+            return audioFile;
     }
+//todo add limit of the number of sessions to downloading
+//todo refactoring (think about architecture and UI)
+//todo add Singleton for logging
         /**
          * grabs the status text and saves that into status.txt file
          *
@@ -131,7 +156,6 @@ public class Main {
             Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("status.txt"), "utf-8"));
             writer.write(text);
             writer.close();
-
         }
 
         /**
@@ -144,6 +168,11 @@ public class Main {
             FileUtils.copyFile(scrFile, new File("screenshot.png"));
         }
 
+        public void waiter(String SomeLocatorByXpath){
+             WebDriverWait waitForOne = new WebDriverWait(driver, 10);
+             waitForOne.until(ExpectedConditions.presenceOfElementLocated(By.xpath(SomeLocatorByXpath)));
+          }
+
         public void closeBrowser() {
             driver.close();
         }
@@ -151,7 +180,9 @@ public class Main {
         public static void main(String[] args) throws IOException, InterruptedException {
             Main webSrcapper = new Main();
             webSrcapper.openTestSite();
-            webSrcapper.findAllThumbnails();
+            // if we have not episodes then quit
+            if (!webSrcapper.findAllThumbnails()) { return; }
+            webSrcapper.downloadList();
 
             //webSrcapper.findThumbnail();
 
